e5ce15ba65d0e7f6fb758e72d1c07ced7dd48bd6
nazgulnurtaeva1 <nazgulnurtaeva1@gmail.com>
2022-02-01 14:53:52 -0500

    The Basic structure is complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         SolarSystem milkyway = new SolarSystem();
+milkyway.sun=1;
+milkyway.Moon=3;
+milkyway.Planets=9;
 
 
 
